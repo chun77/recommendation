cc0f3553492fb293100e7d38078f92fcaadfcdb9
haruka_pointer <29192189@qq.com>
2022-05-05 20:08:13 +0800

    update matrix set and get

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ import matrix.sparse_matrix as mat
 # import matrix.sparse_vector as vec
 if __name__ == '__main__':
     a_index={1:[1],2:[1,2]}
-    a_value={1:[2],2:[2,3]}
-    b_index={1:[1]}
-    b_value={1:[2]}
+    a_value={1: {1:2},2:{1:2,2:3}}
+    b_index={1:[1],2:[2]}
+    b_value={1:{1:2},2:{2:1}}
     c_value={1:2,2:1}
     mat3=mat.SparseVector(c_value,2)
-    mat3.print()
+    # mat3.print()
     mat1=mat.SparseMatrix(2,2,a_index,a_value)
-    mat2=mat.SparseMatrix(2,1,b_index,b_value)
+    mat2=mat.SparseMatrix(2,2,b_index,b_value)
     mat1.print()
-    mat2.print()
-    res=mat1*3
-    res.print()
-    res=mat3*2
-    res.print()
-    # res=mat1-mat2
+    mat1.set(1,2,5)
+    mat1.print()
+    # mat2.print()
+    # res=mat1*3
+    # res.print()
+    # res=mat3*2
+    # res.print()
+    # res=mat1*3
     # res.print()
--- matrix/sparse_matrix.py
+++ matrix/sparse_matrix.py
@@ -63,13 +63,13 @@ class SparseMatrix:
                  row=1000,
                  col=1000,
                  index={i:list() for i in range(1,1001)},
-                 value={i:list() for i in range(1,1001)}):
+                 value={i:dict() for i in range(1,1001)}):
         """
 
         :param row:
         :param col:
         :param index: dict,e.g.[1:[1,2,3],2:[2,3]],index of matrix row and col which value isn't 0
-        :param value: dict,e.g.[1:[1,1,1],2:[1,1]],value of matrix
+        :param value: dict,e.g.[1:{1:1,2:1,3:1},2:{2:3,3:1}],value of matrix
         """
         self.row_len=row
         self.col_len=col
@@ -77,23 +77,23 @@ class SparseMatrix:
         self.value=value
 
     def __add__(self, other):
-        if self.col_len!=other.col_len or self.row_len!=other.row_len:
-            raise(IndexError)
+        # if self.col_len!=other.col_len or self.row_len!=other.row_len:
+        #     raise(IndexError)
         res_index=self.index.copy()
         res_value=self.value.copy()
         for i in other.index:   # row
-            for j,value_j in enumerate(other.index[i]):   # col
-                if value_j in res_index[i]:   # value in res not 0
+            for j in other.index[i]:   # col
+                if j in res_index[i]:   # value in res not 0
                     res_value[i][j]+=other.value[i][j]
                 else:
                     res_index[i].append(value_j)
-                    res_value[i].append(other.value[i][j])
-
+                    res_value[i][j]=other.value[i][j]
         for i in res_index:
-            for j in range(len(res_index[i])):
+            for j in res_index[i]:
                 if res_value[i][j]==0:
-                    res_index[i][j].pop()
-                    res_value[i][j].pop()
+                    # delete 0
+                    res_index[i].remove(j)  # list delete value
+                    res_value[i].pop(j)    # dict delete key
         res_matrix=SparseMatrix(self.row_len,self.col_len,res_index,res_value)
         return res_matrix
 
@@ -104,18 +104,18 @@ class SparseMatrix:
         res_index=self.index.copy()
         res_value=self.value.copy()
         for i in other.index:   # row
-            for j,value_j in enumerate(other.index[i]):   # col
-                if value_j in res_index[i]:   # value in res not 0
+            for j in other.index[i]:   # col
+                if j in res_index[i]:   # value in res not 0
                     res_value[i][j]-=other.value[i][j]
                 else:
                     res_index[i].append(value_j)
-                    res_value[i].append(-other.value[i][j])
-
+                    res_value[i][j]=-other.value[i][j]
         for i in res_index:
-            for j in range(len(res_index[i])):
+            for j in res_index[i]:
                 if res_value[i][j]==0:
-                    res_index[i][j].pop()
-                    res_value[i][j].pop()
+                    # delete 0
+                    res_index[i].remove(j)  # list delete value
+                    res_value[i].pop(j)    # dict delete key
         res_matrix=SparseMatrix(self.row_len,self.col_len,res_index,res_value)
         return res_matrix
 
@@ -128,27 +128,26 @@ class SparseMatrix:
         """
         if isinstance(other,SparseMatrix):
             res_index = {i:list() for i in range(1,self.row_len+1)}
-            res_value = {i:list() for i in range(1,self.row_len+1)}
+            res_value = {i:dict() for i in range(1,self.row_len+1)}
             for i in self.index:
-                for j,col in enumerate(self.index[i]):
+                for j in self.index[i]:
                     for other_i in other.index:
-                        for other_j,other_col in enumerate(other.index[other_i]):
-                            if col==other_i:
+                        for other_j in other.index[other_i]:
+                            if j==other_i:
                                 value=self.value[i][j]*other.value[other_i][other_j]
-                                if other_col in res_index[i]:
-                                    new_index=res_index[i].index(other_col)
-                                    res_value[i][new_index]+=value
+                                if other_j in res_index[i]:
+                                    res_value[i][other_j]+=value
                                 else:
-                                    res_index[i].append(other_col)
-                                    res_value[i].append(value)
+                                    res_index[i].append(other_j)
+                                    res_value[i][other_j]=value
             res_matrix=SparseMatrix(self.row_len,other.col_len,res_index,res_value)
             return res_matrix
         elif isinstance(other,SparseVector):
             res = dict()
             for i in self.index:
-                for j,col in enumerate(self.index[i]):
+                for j in self.index[i]:
                     for other_i in other.value:
-                        if col==other_i:
+                        if j==other_i:
                             value=self.value[i][j]*other.value[other_i]
                             if i in res:
                                 res[i] += value
@@ -160,7 +159,7 @@ class SparseMatrix:
             res_index = self.index.copy()
             res_value = self.value.copy()
             for i in res_value:
-                for j in range(len(res_value[i])):
+                for j in res_value[i]:
                     res_value[i][j]*=other
             res_matrix=SparseMatrix(self.row_len,self.col_len,res_index,res_value)
             return res_matrix
@@ -175,19 +174,36 @@ class SparseMatrix:
         """
         res=0
         if is_abs:
-            for i in self.value:
-                res+=abs(i)
+            for i in self.index:
+                for j in self.index[i]:
+                    res+=abs(self.value[i][j])
         else:
-            for i in self.value:
-                res+=i
+            for i in self.index:
+                for j in self.index[i]:
+                    res += self.value[i][j]
         return sum
 
     def print(self):
         out_matrix=np.zeros([self.row_len,self.col_len])
         for i in self.index:
             row=i
-            for j,list_j in enumerate(self.index[i]):
-                col=self.index[i][j-1]
-                value=self.value[i][j-1]
+            for j in self.index[i]:
+                col=j
+                value=self.value[i][j]
                 out_matrix[row-1][col-1]=value
         print(out_matrix)
+
+
+    def set(self,i,j,value):
+        if j in self.index[i]:
+            self.value[i][j]=value
+        else:
+            self.index[i].append(j)
+            self.value[i][j]=value
+
+
+    def get(self,i,j):
+        if j in self.index[i]:
+            return self.value[i][j]
+        else:
+            raise IndexError
