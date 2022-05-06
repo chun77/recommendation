f3113747144725bf2f3a3da6d6c9d2bffafd5a52
Wang Zichun <1433816968@qq.com>
2022-05-06 13:21:26 +0800

    rearrange files

--- DataReadWrite/readWriteBlock.py
+++ DataReadWrite/readWriteBlock.py
@@ -3,19 +3,19 @@ import sys
 
 
 def get_matrixb(i0, j0):
-    with open('DataReadWrite/block' + str(i0) + '_' + str(j0) + '.pkl', 'rb') as ff:
+    with open('DataReadWrite/matrixBlocks/block' + str(i0) + '_' + str(j0) + '.pkl', 'rb') as ff:
         mb = pickle.load(ff)
     return mb
 
 
 def get_vector(i):
-    with open('DataReadWrite/oldVector' + str(i) + '.pkl', 'rb') as ff:
+    with open('DataReadWrite/vectorBlocks/oldVector' + str(i) + '.pkl', 'rb') as ff:
         vb = pickle.load(ff)
     return vb
 
 
 def set_vector(i, vb):
-    with open('DataReadWrite/oldVector' + str(i) + '.pkl', 'wb') as ff:
+    with open('DataReadWrite/vectorBlocks/oldVector' + str(i) + '.pkl', 'wb') as ff:
         pickle.dump(ff, vb)
     return
 
